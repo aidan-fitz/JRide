2d841eba383dde864c36b55c570bf347a6e846b2
Benjamin Lanier <benlanier24@gmail.com>
2015-01-08 12:18:49 -0500

    The Algorithm for the velocity of the cart

---
+++ VelocityAlgorithm.java
@@ -0,0 +1,32 @@
+public class VelocityAlgorithm {
+    private double Mass, Velocity, Acceleration, Friction, Angle, Slope, KE, PE,Hieght, MechE, Gravity;
+    public void SetHieght(int[] Coordinates, int i
++ndex){	
+	this.Hieght = Coordinates[index];
+    }
+
+    public void SetMechanicalEnergy(double Mechanical){
+	this.MechE = Mechanical;
+    }
+    public int CalculatePotential(){
+   this.PE = Hieght * Gravity * Mass;
+    return PE;
+}
+
+public int CalculateKinectic(){
+ this.KE = .5 * (Velocity)^ 2 *mass;
+	return KE;
+
+public int VeloctiytoDistance(){
+
+}
+	
+public void Driver(){
+
+
+
+}
+
+
+
+}
